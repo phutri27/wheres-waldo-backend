d1c5a5123a47d98c5c62bf626ad52e25b25428c6
phutri27 <triphu27@gmail.com>
2026-02-07 14:26:15 +0000

    add scoreboarc controller

---
+++ src/controllers/scoreboardController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express"
+import { scoreboardObj } from "../queries/script"
+
+export const addToScoreboard = async (req: Request, res: Response) => {
+    const { username } = req.body
+    const map_id = Number(req.body.map_id)
+    const score = Number(req.body.score)
+    try {
+        await scoreboardObj.postScoreboard(map_id, username, score)
+        return res.status(200).json({message: "Add to scoreboard succesfully"})
+    } catch (error) {
+        return res.status(401).json({message: "Cannot query database"})
+    }   
+}
+
+export const getScoreboard = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id)
+        const result = await scoreboardObj.getScoreboard(id)
+        return res.status(200).json(result)
+    } catch (error) {
+        return res.status(401).json({message: "Cannot query database"})
+    }
+}
