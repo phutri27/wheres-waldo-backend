f3df01f35cc42148c1d4e0eae9eaea043b96905b
phutri27 <triphu27@gmail.com>
2026-02-07 14:23:31 +0000

    Initialize server

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express"
+import "dotenv/config"
+import routes from "./routes/index"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.use("/", routes.coord)
+app.use("/scoreboard", routes.scoreboard)
+
+app.listen(3000, () => {
+  console.log(`Server running on http://localhost:${3000}`);
+});
