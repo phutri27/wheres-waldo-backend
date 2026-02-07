801d1a88b2eba1ef82e1881bda6464ea30b7fa34
phutri27 <triphu27@gmail.com>
2026-02-07 14:23:49 +0000

    Create prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../../generated/prisma/client'
+
+const connectionString = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
