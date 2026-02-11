08949002f7b97e39c288e5c7222de23c41f7122d
phutri27 <triphu27@gmail.com>
2026-02-11 19:10:55 +0000

    fix: remove username length validate

--- src/validators/validate.ts
+++ src/validators/validate.ts
@@ -8,8 +8,6 @@ export const validateUsernane = [
     .trim()
     .notEmpty()
     .withMessage(`Username ${emptyMsg}`)
-    .isLength({max: 50})
-    .withMessage('Username must not exceed 50 characters')
     .custom(async (value, {req}) => {
         const result = await scoreboardObj.getUsername(value)
         if (result){
